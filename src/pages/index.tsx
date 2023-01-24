4669acc638f55b4bacf529a12c2622b319c2a114
Andres2D <andres2d1997.1206@gmail.com>
2023-01-23 21:37:46 -0500

    Add about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import type { HeadFC } from "gatsby";
 import Intro from "../components/intro";
+import About from "../components/about";
 
 const IndexPage: React.FC = () => {
   return (
     <>
       <Intro />
+      <About />
     </>
   )
 }
