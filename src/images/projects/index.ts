0a685bc68bf0aadb9647ae6c2b23ad2dc2dfd7be
Andres2D <andres2d1997.1206@gmail.com>
2023-01-24 23:08:09 -0500

    Add index to get the assets in an easy way

---
+++ src/images/projects/index.ts
@@ -0,0 +1,21 @@
+import blog from './2d-blog.svg';
+import cotejoApp from './cotejo-app.svg';
+import countryApp from './country-app.svg';
+import diceGame from './dice-game.svg';
+import dontGuess from './dont-guess-app.svg';
+import gifsApp from './gifs-app.svg';
+import landingPage from './landing-page.svg';
+import mapsApp from './maps-app.svg';
+import motoFox from './moto-fox-app.svg';
+
+export {
+  blog,
+  cotejoApp,
+  countryApp,
+  diceGame,
+  dontGuess,
+  gifsApp,
+  landingPage,
+  mapsApp,
+  motoFox,
+}
