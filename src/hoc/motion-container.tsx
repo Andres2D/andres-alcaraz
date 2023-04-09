36e337d0b1572bd85ea0b16a95ed0f0ba5e5815f
Andres2D <andres2d1997.1206@gmail.com>
2023-04-09 15:24:41 -0500

    Add hoc to wrap motion configurations

---
+++ src/hoc/motion-container.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { staggerContainer } from '../utils/motion';
+
+export const MotionWrapper = (Component: React.FC) => 
+  function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer()}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: true, amount: 0.25 }}
+      >
+        <Component />
+      </motion.section>
+    )
+  };
