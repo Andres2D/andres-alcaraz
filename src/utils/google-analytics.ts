6015500c010317a4941c814147aa87f043eb664f
Andres2D <andres2d1997.1206@gmail.com>
2024-02-13 00:31:55 -0500

    Configure basic tracking on enter page

---
+++ src/utils/google-analytics.ts
@@ -0,0 +1,6 @@
+import ReactGA from 'react-ga';
+
+export const initializeGoogleAnalytics = (): void => {
+  ReactGA.initialize(process.env.GATSBY_GOOGLE_ANALYTICS_MEASUREMENT_ID || '');
+  ReactGA.pageview('/full-page');
+};
