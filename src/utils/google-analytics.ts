2fe591e32a864eea5bc421850fdb9e876fb7e434
Andres2D <andres2d1997.1206@gmail.com>
2024-02-15 17:23:06 -0500

    Add static event when user visits page

--- src/utils/google-analytics.ts
+++ src/utils/google-analytics.ts
@@ -3,4 +3,8 @@ import ReactGA from 'react-ga';
 export const initializeGoogleAnalytics = (): void => {
   ReactGA.initialize(process.env.GATSBY_GOOGLE_ANALYTICS_MEASUREMENT_ID || '');
   ReactGA.pageview('/full-page');
+  ReactGA.event({
+    category: 'User',
+    action: 'Visit main page'
+  });
 };
