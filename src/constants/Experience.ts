94a353bed20bab0e29acbcf6c4a11aca1b63c273
Andres2D <andres2d1997.1206@gmail.com>
2024-04-19 21:06:14 -0400

    refactor constants and interfaces for new experience format

---
+++ src/constants/Experience.ts
@@ -0,0 +1,57 @@
+import { Experience } from "../interfaces/Experience";
+
+export const experience: Experience[] = [
+  {
+    id: 1,
+    place: 'Accenture ltda',
+    title: 'Senior Frontend Developer',
+    from: 2021,
+    to: 2024,
+    description: 'Techs solutions, mono-repositories with Nx, Angular, Jest, Strapi CMS, Jira, Schematics. ',
+    link: 'https://www.accenture.com/co-es',
+    icon: 'developer'
+  },
+  {
+    id: 6,
+    from: 2024,
+    title: 'CELPIP-GENERAL - Average 10',
+    description: 'Highly effective proficiency in workplace and community contexts',
+    icon: 'language'
+  },
+  {
+    id: 5,
+    from: 2023,
+    title: 'IELTS General Training - Band 7 - C1',
+    description: 'Good User',
+    icon: 'language'
+  },
+  {
+    id: 4,
+    title: 'Software Engineer at',
+    place: 'Politécnico Colombiano Jaime Isaza Cadavid',
+    from: 2021,
+    description: 'Bachelors degree (four years) - Computer Engineering Issued by World Education Services',
+    link: 'https://www.politecnicojic.edu.co/',
+    icon: 'education'
+  },
+  {
+    id: 2,
+    place: 'NumRot',
+    title: 'Web Analyst',
+    from: 2019,
+    to: 2021,
+    description: 'Electronic billing provider, Angular, ASP .Net Core, AWS (S3, Elastic Beanstalk), MySql.',
+    link: 'https://www.numrot.com/',
+    icon: 'developer'
+  },
+  {
+    id: 3,
+    place: 'MotoFox',
+    title: 'Administrative',
+    from: 2015,
+    to: 2018,
+    description: 'Familiar business, administrative position, cashier',
+    link: 'https://motofox-belen.netlify.app/',
+    icon: 'worker'
+  }
+];
