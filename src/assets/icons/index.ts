01d751d04c18fb4d490f3b2d759b807cda20eee2
Andres2D <andres2d1997.1206@gmail.com>
2024-04-19 21:06:44 -0400

    Add new assets and global styles

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,11 @@
+import WorkerIcon from './worker';
+import DeveloperIcon from './developer';
+import LanguageIcon from './language';
+import EducationIcon from './education';
+
+export {
+  WorkerIcon,
+  DeveloperIcon,
+  LanguageIcon,
+  EducationIcon
+}
