646a4c4aa3a8853d9c6d4b9373ffb4b0134d5f0e
Andres2D <andres2d1997.1206@gmail.com>
2025-01-29 17:20:39 -0500

    feat: add posthog integration for tracking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import type { HeadFC } from "gatsby";
 import { Helmet } from 'react-helmet';
+import { PostHogProvider} from 'posthog-js/react';
 import StartsCanvas from '../components/canvas/stars';
 import * as styles from './index.module.scss';
 import {
@@ -15,23 +16,32 @@ import {
 
 const IndexPage: React.FC = () => {
 
+  const trackingOptions = {
+    api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST || ''
+  }
+
   return (
     <>
-      <Helmet>
-        <html lang='en' />
-        <meta name="description" content="Andres Alcaraz, Software Developer" />
-        <link rel="icon" type="image/x-icon" href="icons/icon-48x48.png" />
-      </Helmet>
-      <div className={styles.introContainer}>
-        <Intro />
-        <StartsCanvas />
-      </div>
-      <About />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Chatbot />
+      <PostHogProvider
+        apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY || ''}
+        options={trackingOptions}
+      >
+        <Helmet>
+          <html lang='en' />
+          <meta name="description" content="Andres Alcaraz, Software Developer" />
+          <link rel="icon" type="image/x-icon" href="icons/icon-48x48.png" />
+        </Helmet>
+        <div className={styles.introContainer}>
+          <Intro />
+          <StartsCanvas />
+        </div>
+        <About />
+        <Experience />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Chatbot />
+      </PostHogProvider>
     </>
   )
 }
